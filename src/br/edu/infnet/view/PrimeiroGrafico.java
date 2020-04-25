c0c91c2064e13a51bc7d394ebba109c2903164ac
brunorjpl <brunorjpl@gmail.com>
2020-04-25 17:29:03 -0300

    lendo csv

--- src/br/edu/infnet/view/PrimeiroGrafico.java
+++ src/br/edu/infnet/view/PrimeiroGrafico.java
@@ -1,5 +1,11 @@
 package br.edu.infnet.view;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import javax.swing.JFrame;
 
 import org.jfree.chart.ChartFactory;
@@ -9,6 +15,11 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import br.edu.infnet.model.LerDados;
+
 public class PrimeiroGrafico extends JFrame{
 
 	
@@ -17,21 +28,36 @@ public class PrimeiroGrafico extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public PrimeiroGrafico() {
+	public PrimeiroGrafico() throws IOException {
 	      super( "Projeto de Bloco" ); 
 	      
-	    //Cria um dataSet para inserir os dados que serão passados para a criação do gráfico tipo Pie
-	      DefaultCategoryDataset ds = new DefaultCategoryDataset();
-	       
-	   // cria o conjunto de dados
-	    
-	      ds.addValue(10.5, "maximo", "dia 1");
-	      ds.addValue(28.2, "maximo", "dia 2");
-	      ds.addValue(37.3, "maximo", "dia 3");
-	      ds.addValue(41.5, "maximo", "dia 4");
-	      ds.addValue(35.7, "maximo", "dia 5");
-	      ds.addValue(82.5, "maximo", "dia 6");
-	       
+//	    //Cria um dataSet para inserir os dados que serão passados para a criação do gráfico 
+//	      DefaultCategoryDataset ds = new DefaultCategoryDataset();
+//	       
+//	   // cria o conjunto de dados
+//	    
+//	      ds.addValue(10.5, "maximo", "dia 1");
+//	      ds.addValue(28.2, "maximo", "dia 2");
+//	      ds.addValue(37.3, "maximo", "dia 3");
+//	      ds.addValue(41.5, "maximo", "dia 4");
+//	      ds.addValue(35.7, "maximo", "dia 5");
+//	      ds.addValue(82.5, "maximo", "dia 6");
+//	       
+	  	
+			DefaultCategoryDataset ds = new DefaultCategoryDataset();
+			
+		    Reader reader = Files.newBufferedReader(Paths.get("MGLU3.csv"));
+	
+	        CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
+
+	        List<String[]> valores = csvReader.readAll();
+
+	        for (String[] valor : valores) {
+	            ds.addValue(Double.parseDouble(valor[4]), "fechamento", valor[0]);
+	            System.out.println("Date : " + valor[0] +
+                        " - Close : " + valor[4]);
+	        }
+	      
 	      //Cria um objeto JFreeChart passando os seguintes parametros
 	      JFreeChart grafico = ChartFactory.createLineChart("MGLU3.SA", "Dia", 
 	    		    "Valor", ds, PlotOrientation.VERTICAL, true, true, false);
@@ -41,7 +67,7 @@ public class PrimeiroGrafico extends JFrame{
 	      this.pack();
 	   }
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		new PrimeiroGrafico().setVisible(true);
 		
